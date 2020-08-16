591ea383353acf38ae1ada5980adadf927e47d4c
Michael Pittelko <mike@pittelko.net>
2020-08-15 19:25:49 -0500

    initial reedsolom example

--- cmd/sar/main.go
+++ cmd/sar/main.go
@@ -2,8 +2,34 @@ package main
 
 
 import "fmt"
+import "math/rand"
 import "github.com/klauspost/reedsolomon"
 
 func main() {
-	fmt.Println("Hello")
+	// Create some sample data
+	var data = make([]byte, 250000)
+
+	for i := range data {
+		data[i] = byte(rand.Intn(8))
+	}
+
+	enc, _ := reedsolomon.New(7, 3)
+
+	shards, _ := enc.Split(data)
+
+	_ = enc.Encode(shards)
+
+	ok, _ := enc.Verify(shards)
+	if ok {
+		fmt.Println("Verify ok")
+	}
+
+	shards[4], shards[6] = nil, nil
+
+	_ = enc.Reconstruct(shards)
+
+	ok, _ = enc.Verify(shards)
+	if ok {
+		fmt.Println("Reconstruct ok")
+	}
 }
