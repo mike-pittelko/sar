c97e2b3377ee6f1d5a7f17f15bdf1f8c7a32cc2e
Michael Pittelko <mike@pittelko.net>
2020-11-21 20:51:09 -0600

    Add concept of a slab -slab = original data + encoded data

--- cmd/sar/go.mod
+++ cmd/sar/go.mod
@@ -1,5 +1,10 @@
 module github.com/mike-pittelko/sar
 
-go 1.14
+go 1.15
 
 require github.com/klauspost/reedsolomon v1.9.9
+
+require (
+	github.com/google/uuid v1.1.2
+	github.com/klauspost/cpuid v1.3.1
+)
--- cmd/sar/main.go
+++ cmd/sar/main.go
@@ -1,34 +1,63 @@
 package main
 
-import "fmt"
-import "math/rand"
-import "github.com/klauspost/reedsolomon"
+import (
+	"fmt"
+
+	"math/rand"
+
+	"crypto/sha1"
+
+	"github.com/google/uuid"
+	"github.com/klauspost/reedsolomon"
+)
+
+type slabHeader struct {
+	slabIdent string
+	slabHash  [sha1.Size]byte
+}
+
+// SlabSize ... : Default size of a slab
+const SlabSize = 250000 // Default size of a slab
+
+type slab struct {
+	header    slabHeader
+	slabBytes []byte
+}
+
+//TODO: Make slab a container for the slab, and the shards, figure our how "new" should work on a struct
 
 func main() {
 	// Create some sample data
-	var data = make([]byte, 250000)
 
-	for i := range data {
-		data[i] = byte(rand.Intn(8))
+	var slab1 = new(slab)
+	slab1.header.slabIdent = uuid.New().String()
+	slab1.slabBytes = make([]byte, SlabSize)
+	slab1.header.slabHash = sha1.Sum(slab1.slabBytes)
+	//	slab1.header.slabHash.Sum(slab1.slabBytes)
+
+	for i := range slab1.slabBytes {
+		slab1.slabBytes[i] = byte(rand.Intn(255))
 	}
 
-	enc, _ := reedsolomon.New(7, 3)
+	enc, _ := reedsolomon.New(7, 4)
 
-	shards, _ := enc.Split(data)
+	shards, _ := enc.Split(slab1.slabBytes)
 
 	_ = enc.Encode(shards)
 
 	ok, _ := enc.Verify(shards)
 	if ok {
-		fmt.Println("Verify ok")
+		fmt.Println("SAR: Verify ok")
 	}
 
-	shards[4], shards[6] = nil, nil
-
+	shards[4], shards[6], shards[7], shards[3] = nil, nil, nil, nil
+	fmt.Println("SAR: Destroyed shards 4,6,7,3 (7 shards, 4 destroyed)")
 	_ = enc.Reconstruct(shards)
 
 	ok, _ = enc.Verify(shards)
 	if ok {
-		fmt.Println("Reconstruct ok")
+		fmt.Println("SAR: Reconstruct ok")
+	} else {
+		fmt.Println("SAR: Failed to reconstruct")
 	}
 }
