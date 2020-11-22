f4cc3b9f089c3f8fe875440fe848ceadcc8c155b
Michael Pittelko <mike@pittelko.net>
2020-11-22 16:07:45 -0600

    Rework module/pkg setup

--- cmd/sar/main.go
+++ cmd/sar/main.go
@@ -1,63 +1,41 @@
 package main
 
 import (
-	"fmt"
-
-	"math/rand"
-
-	"crypto/sha1"
-
-	"github.com/google/uuid"
-	"github.com/klauspost/reedsolomon"
+	"github.com/mike-pittelko/sar/slab"
 )
 
-type slabHeader struct {
-	slabIdent string
-	slabHash  [sha1.Size]byte
-}
-
-// SlabSize ... : Default size of a slab
-const SlabSize = 250000 // Default size of a slab
-
-type slab struct {
-	header    slabHeader
-	slabBytes []byte
-}
-
 //TODO: Make slab a container for the slab, and the shards, figure our how "new" should work on a struct
+//TODO: Existing slab definition is more like a shard than a slab. shard needs, header, sum, etc.
 
 func main() {
 	// Create some sample data
 
-	var slab1 = new(slab)
-	slab1.header.slabIdent = uuid.New().String()
-	slab1.slabBytes = make([]byte, SlabSize)
-	slab1.header.slabHash = sha1.Sum(slab1.slabBytes)
-	//	slab1.header.slabHash.Sum(slab1.slabBytes)
+	var slab1 = slab.NewSlabWithSize(250000)
+	slab1.RandomFill()
 
-	for i := range slab1.slabBytes {
-		slab1.slabBytes[i] = byte(rand.Intn(255))
-	}
+	/*
+		enc, _ := reedsolomon.New(7, 4)
 
-	enc, _ := reedsolomon.New(7, 4)
+			shards, _ := enc.Split(slab1.slabBytes)
 
-	shards, _ := enc.Split(slab1.slabBytes)
+			_ = enc.Encode(shards)
 
-	_ = enc.Encode(shards)
+			ok, _ := enc.Verify(shards)
+			if ok {
+				fmt.Println("SAR: Verify ok")
+			}
 
-	ok, _ := enc.Verify(shards)
-	if ok {
-		fmt.Println("SAR: Verify ok")
-	}
+			shards[4], shards[6], shards[7], shards[3] = nil, nil, nil, nil
+			fmt.Println("SAR: Destroyed shards 4,6,7,3 (7 shards, 4 destroyed)")
+			_ = enc.Reconstruct(shards)
 
-	shards[4], shards[6], shards[7], shards[3] = nil, nil, nil, nil
-	fmt.Println("SAR: Destroyed shards 4,6,7,3 (7 shards, 4 destroyed)")
-	_ = enc.Reconstruct(shards)
+			ok, _ = enc.Verify(shards)
+			if ok {
+				fmt.Println("SAR: Reconstruct ok")
+			} else {
+				fmt.Println("SAR: Failed to reconstruct")
+			}
 
-	ok, _ = enc.Verify(shards)
-	if ok {
-		fmt.Println("SAR: Reconstruct ok")
-	} else {
-		fmt.Println("SAR: Failed to reconstruct")
-	}
+	*/
+
 }
---
+++ cmd/sar/slab/slab.go
@@ -0,0 +1,59 @@
+package slab 
+
+import (
+	"crypto/sha1"
+	"math/rand"
+
+	"github.com/google/uuid"
+)
+
+// SlabHeader is the header for a slab, include the identity of the slab, the hash of the slab and pointers to the shards.
+type SlabHeader struct {
+	slabIdent string
+	slabHash  [sha1.Size]byte
+}
+
+// ShardHeader is the header for a single shard, a slice of a slab, that is to be managed with error coding
+type ShardHeader struct {
+	shardIdent string
+	shardHash  [sha1.Size]byte
+}
+
+// Slab is a file like object that contains the raw data, and the shards ready to be sent.
+type Slab struct {
+	header    SlabHeader
+	slabBytes []byte
+}
+
+// Shard is a segment of a slab
+type Shard struct {
+	header     ShardHeader
+	shardBytes []byte
+}
+
+// NewSlab is a factory function or empty slabs that are initialized with a UUID only.
+func NewSlab() *Slab {
+	slab1 := new(Slab)
+	slab1.header.slabIdent = uuid.New().String()
+	return slab1
+}
+
+// NewSlabWithSize is a factory function for slabs that have a buffer preallocates with newSize uint64 bytes of zeros.
+func NewSlabWithSize(newSize uint64) *Slab {
+	slab1 := NewSlab()
+	slab1.slabBytes = make([]byte, newSize)
+	slab1.header.slabHash = sha1.Sum(slab1.slabBytes)
+	return slab1
+}
+
+// RandomFill fills a slab (leaving shards untouched) to it's current size with random bytes. This is normally only useful in debugging scenarios
+func (x *Slab) RandomFill() *Slab {
+	for i := range x.slabBytes {
+		x.slabBytes[i] = byte(rand.Intn(255))
+	}
+	x.header.slabHash = sha1.Sum(x.slabBytes)
+	return x
+}
+
+//TODO: Make slab a container for the slab, and the shards, figure our how "new" should work on a struct
+//TODO: Existing slab definition is more like a shard than a slab. shard needs, header, sum, etc.
