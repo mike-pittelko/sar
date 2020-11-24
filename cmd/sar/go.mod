56fb9c39fae4089b80626e7f5b097586f3639a60
Michael Pittelko <mike@pittelko.net>
2020-11-24 12:45:05 -0600

    Upate files

--- cmd/sar/go.mod
+++ cmd/sar/go.mod
@@ -6,7 +6,7 @@ require github.com/klauspost/reedsolomon v1.9.9
 
 require (
 	github.com/google/uuid v1.1.2
-	github.com/klauspost/cpuid v1.3.1
+	github.com/klauspost/cpuid v1.3.1 // indirect
 	github.com/mmcloughlin/avo v0.0.0-20201105074841-5d2f697d268f // indirect
 	golang.org/x/tools v0.0.0-20201121010211-780cb80bd7fb // indirect
 )
--- cmd/sar/main.go
+++ cmd/sar/main.go
@@ -1,37 +1,15 @@
 package main
 
-//TODO: Make slab a container for the slab, and the shards, figure our how "new" should work on a struct
-//TODO: Existing slab definition is more like a shard than a slab. shard needs, header, sum, etc.
+import (
+	"github.com/mike-pittelko/sar/slab"
+)
 
 func main() {
 	// Create some sample data
 
-	var slab1 = NewSlabWithSize(250000)
+	var slab1 = slab.NewSlabWithSize(250000)
 	slab1.RandomFill()
 
-	/*
-		enc, _ := reedsolomon.New(7, 4)
-
-			shards, _ := enc.Split(slab1.slabBytes)
-
-			_ = enc.Encode(shards)
-
-			ok, _ := enc.Verify(shards)
-			if ok {
-				fmt.Println("SAR: Verify ok")
-			}
-
-			shards[4], shards[6], shards[7], shards[3] = nil, nil, nil, nil
-			fmt.Println("SAR: Destroyed shards 4,6,7,3 (7 shards, 4 destroyed)")
-			_ = enc.Reconstruct(shards)
-
-			ok, _ = enc.Verify(shards)
-			if ok {
-				fmt.Println("SAR: Reconstruct ok")
-			} else {
-				fmt.Println("SAR: Failed to reconstruct")
-			}
-
-	*/
+	slab1.Test()
 
 }
